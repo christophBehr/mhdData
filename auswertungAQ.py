3aeeb48bebb1c31f522f3eca944d06f0258a2508
christophBehr <diagnoseschmerz@web.de>
2020-12-09 11:34:30 +0100

    Create auswertungAQ.py
    Auswertung Leitstelle Tag/Monat/Jahr

---
+++ auswertungAQ.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+from datetime import timedelta
+
+PATHTXT = "exportArchiv.txt"
+BELEGARCHIV = "data/belegArchiv.csv"
+KTWFMS = "data/ktwFMS.csv"
+FAHRTENSTATISTIK = "data/fahrtenStatistik.csv"
+ABRECHNUNG = "data/abrechnung.csv"
+AUSWERTUNGFMS = "data/auswertungFMS.csv"
+AUSWERTUNGSTATISTIK = "data/auswertungStatistik.csv"
