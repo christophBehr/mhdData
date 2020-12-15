5eeb8b6fa6ac63409200666c20a783e3c93ed096
GrunkleStan <christoph.behr@outlook.com>
2020-12-15 12:22:06 +0100

    Kalenderfunktion

--- auswertungAQ.py
+++ auswertungAQ.py
@@ -17,42 +17,65 @@ STATISTIKMHDONGOING = "data/statistikMHDOngoing.csv"
 STATISTIKDRKONGOING = "data/statistikDRKOngoing.csv"
 
 
-mhd = pd.DataFrame({'HiOrg': ['mhd']})
-drk = "drk"
+mhdKTW = 5
+drkKTW = 8
+wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+
 
 def auswertungTag():
-    #MHD
+    
     dataMHD = pd.read_csv(AUSWERTUNGFAHRTENMHD)
-    datum = dataMHD.at[1, "E-Datum"]
-    mhd = "mhd" 
-    mhdLable = {'Datum':[datum], 'HiOrg':[mhd]}
-    frameLable = pd.DataFrame(data = mhdLable)
+    dataDRK = pd.read_csv(AUSWERTUNGFAHRTENDRK)
 
+    datum = dataMHD.at[1, "E-Datum"]
+    datumLable = {'Datum':[datum]}
+    frameLable = pd.DataFrame(data = datumLable)
+
+    #MHD
     frameMHD = pd.DataFrame(dataMHD, columns = ["Fahrten", "I-Fahrten", "Fernfahrten"])
+    frameMHD = frameMHD.rename(columns = {"Fahrten":"Fahrten-MHD", "I-Fahrten":"I-Fahrten-MHD", "Fernfahrten":"Fernfahrten-MHD"})
     mhdTag = frameMHD.sum()
     frameMHDTag = pd.DataFrame(mhdTag)
     frameMHDTag = frameMHDTag.T
 
-    frameMHDTag = frameLable.join(frameMHDTag)
-
-    frameMHDTag.to_csv(TMPTAGMHD)
-    
-
     #DRK
-    dataDRK = pd.read_csv(AUSWERTUNGFAHRTENDRK)
-    datum = dataDRK.at[1, "E-Datum"]
-    drk = "drk" 
-    drkLable = {'Datum':[datum], 'HiOrg':[drk]}
-    frameLable = pd.DataFrame(data = drkLable)
-
     frameDRK = pd.DataFrame(dataDRK, columns = ["Fahrten", "I-Fahrten", "Fernfahrten"])
+    frameDRK = frameDRK.rename(columns = {"Fahrten":"Fahrten-DRK", "I-Fahrten":"I-Fahrten-DRK", "Fernfahrten":"Fernfahrten-DRK"})
     drkTag = frameDRK.sum()
     frameDRKTag = pd.DataFrame(drkTag)
     frameDRKTag = frameDRKTag.T
-    frameDRKTag = frameLable.join(frameDRKTag)
+
+    #Fahrten im Schnitt je KTW MHD
+    auslastungMHD = frameMHDTag[["Fahrten-MHD"]]
+    proKTWMHD = auslastungMHD.div(mhdKTW)
+    proKTWMHD = proKTWMHD.rename(columns={"Fahrten-MHD":"Auslastung-MHD"})
+    frameMHDTag = frameMHDTag.join(proKTWMHD)
+
+    #Farhten im Schnitt je KTW DRK
+    auslastungDRK = frameDRKTag[["Fahrten-DRK"]]
+    proKTWDRK = auslastungDRK.div(drkKTW)
+    proKTWDRK = proKTWDRK.rename(columns={"Fahrten-DRK":"Auslastung-DRK"})
+    frameDRKTag = frameDRKTag.join(proKTWDRK)
+
+    frameTag = frameMHDTag.join(frameDRKTag)
+    frameTag = frameLable.join(frameTag)
     
+    frameTag["Fahrten Gesamt"] = frameTag["Fahrten-MHD"] + frameTag["Fahrten-DRK"]
+    frameTag["i-Fahrten Gesamt"] = frameTag["I-Fahrten-MHD"] + frameTag["I-Fahrten-DRK"]
+    
+    frameTag["Datum"] = pd.to_datetime(frameTag.Datum)
+    tag = wochentage[int(frameTag.Datum.dt.weekday)]
+    frameTag.insert(loc=0, column = "Wochentag", value = tag)
+
+    print(frameTag)
+    
+    
+
+    
+    frameMHDTag.to_csv(TMPTAGMHD)
     frameDRKTag.to_csv(TMPTAGDRK)
-    writeToArchive(TMPTAGMHD, STATISTIKMHDONGOING)
-    writeToArchive(TMPTAGDRK, STATISTIKDRKONGOING)
+    #writeToArchive(TMPTAGMHD, STATISTIKMHDONGOING)
+    #writeToArchive(TMPTAGDRK, STATISTIKDRKONGOING)
+    return(frameTag)
 
 auswertungTag()
